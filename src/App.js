e9cf8bd41a3b4c49f45c68032a43e10744ed8a71
Iamc0d3r <42385110+Iamc0d3r@users.noreply.github.com>
2021-07-28 20:34:47 +0530

    TODO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Todolist from './components/Todolist';
+import Form from './components/Form';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status,setStatus] = useState('all');
+  const [filteredTodos, setFilterTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos(); //retrieve saved todo list from localStorage
+  }, []);
+  
+  useEffect(() => {
+    filterHandler(); //filters task which is completed or uncompleted
+    saveLocalTodos(); //saving todo in localStorage
+  }, [todos, status]);
+
+
+
+
+  //to check what is completed or not completed in in single view insted of reading all together
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilterTodos(todos.filter(todo => todo.completed === true));       
+        break;
+      case 'uncompleted':
+        setFilterTodos(todos.filter(todo => todo.completed === false));       
+        break;
+      default:
+        setFilterTodos(todos);
+        break;
+    }
+  };
+// saving todo in localStorage
+    const saveLocalTodos = () => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    };
+//retrieve saved todo list from localStorage
+    const getLocalTodos = () => {
+        if(localStorage.getItem('todos') === null){
+            localStorage.setItem('todos', JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem('todos'));
+            setTodos(todoLocal)
+        }
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>To-Do</h1>
       </header>
+      <Form //input field for todo and filter to check task completed or uncompleted.
+      inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      setStatus={setStatus} 
+      />
+      <Todolist //list of todos, 
+      filteredTodos={filteredTodos}
+      setTodos={setTodos} 
+      todos={todos} />
     </div>
   );
 }
 
 export default App;
+ 
